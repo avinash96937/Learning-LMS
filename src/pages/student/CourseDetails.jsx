a12a1951d2e7be7156f28a5573a1d718a5e7712c
Your Name <your-email@example.com>
2025-02-09 22:56:29 +0530

    add courseDetails

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -4,10 +4,14 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import CourseCard from "../../components/student/CourseCard";
 import { assets } from "../../assets/assets";
+import humanizeDuration from "humanize-duration";
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [openSections, setOpenSections] = useState({})
+
+
   const {
     allCourses,
     calculateRating,
@@ -24,6 +28,15 @@ const CourseDetails = () => {
   useEffect(() => {
     fetchCourseData();
   }, [courseData]);
+
+  const toggleSection = (index) => {
+    setOpenSections((prev) => (
+      { ...prev, [index]: !prev[index], }
+    ))
+
+  }
+  console.log(openSections);
+
 
 
   return courseData ? (
@@ -75,26 +88,74 @@ const CourseDetails = () => {
           <div className=" pt-8 text-gray-800">
             <h2 className=" text-xl font-semibold">Course Structure</h2>
             <div className=" pt-5">
-                  {courseData.courseContent.map((chapter, index)=>(
-                    <div key={index}>
-                      <div>
-                        <div>
-                          <img src={assets.down_arrow_icon} alt="arrow icon" />
-                          <p>{chapter.chapterTitle}</p>
-                        </div>
-                        <p>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
-                      </div>
+              {courseData.courseContent.map((chapter, index) => (
+                <div
+                  key={index}
+                  className=" border border-gray-300 bg-white mb-2 rounded"
+                >
+                  <div onClick={() => toggleSection(index)} className=" flex items-center justify-between px-4 py-3 cursor-pointer select-none ">
+                    <div className=" flex items-center gap-2">
+                      <img className={`transform transition-transform ${openSections[index] ? 'rotate-180' : ''}`} src={assets.down_arrow_icon} alt="arrow icon" />
+                      <p className=" font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
                     </div>
-                  ))}
+                    <p className="text-sm md:text-default">
+                      {chapter.chapterContent.length} lectures -{" "}
+                      {calculateChapterTime(chapter)}
+                    </p>
+                  </div>
+
+                  <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' : 'max-h-0'}`}>
+                    <ul className=" list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, i) => (
+                        <li key={i} className=" flex items-start gap-2 py-1">
+                          <img
+                            src={assets.play_icon}
+                            alt="play icon"
+                            className=" w-4 h-4 mt-1"
+                          />
+                          <div className=" flex items-center justify-between w-full text-gray-800 text-xs md:text-default ">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className=" flex gap-2">
+                              {lecture.isPreviewFree && <p className="text-blue-500 cursor-pointer">Preview</p>}
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
             </div>
+
+            <div className=" py-20 text-sm md:text-default">
+              <h3 className=" text-xl font-semibold text-gray-800">Course Description</h3>
+              <p
+                className=" pt-3 rich-text"
+                dangerouslySetInnerHTML={{
+                  __html: courseData.courseDescription
+                }}
+              ></p>
+            </div>
+
           </div>
         </div>
         {/* Right Column */}
-        <div>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto labore
-          perferendis libero cum culpa, dignissimos inventore illo eaque eveniet
-          in ad architecto explicabo illum. Laborum exercitationem quasi nostrum
-          magni vero!
+        <div className=" z-10 max-w-course-card shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px} sm:min-w-[420px]">
+          <img src={courseData.courseThumbnail} alt="" />
+          <div className=" pt-5 ">
+            <div>
+              <img className="w-3.5" src={assets.time_left_clock_icon} alt="" />
+              <p className="text-red-500"><span className=" font-medium">5 days</span> left at this price!</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
@@ -104,3 +165,6 @@ const CourseDetails = () => {
 };
 
 export default CourseDetails;
+
+
+
